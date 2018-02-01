c3a59143fc97ddffcbe06589ed5c841151f63ecb
JonSnow13 <manuelortegajr1997@gmail.com>
2018-02-01 18:04:39 +0800

    test

--- TerheaMed/resources/views/welcome.blade.php
+++ TerheaMed/resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel Wassup
                 </div>
 
                 <div class="links">
